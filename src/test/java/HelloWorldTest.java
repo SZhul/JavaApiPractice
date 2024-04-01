25c0c526d02d6855f9550881235070dbbde37585
Sergey Zhul <s.zhul@bftcom.com>
2024-04-01 20:10:43 +0700

    improve get test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,11 +2,20 @@ import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld() {
+    public void testRestAssured() {
+
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Sergey");
+
         Response response = RestAssured
+                .given()
+                .queryParams(params)
                 .get("http://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
