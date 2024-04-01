0eec3956a5ea8662f0fedce4ef8945ec9c12459c
Sergey Zhul <s.zhul@bftcom.com>
2024-04-01 19:45:48 +0700

    Get test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,14 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld() {
-        System.out.println("Hello World");
+        Response response = RestAssured
+                .get("http://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
