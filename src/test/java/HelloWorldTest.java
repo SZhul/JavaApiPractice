7dbfeac440e9e93b31f4431ed87b035c99590a8d
Sergey Zhul <s.zhul@bftcom.com>
2024-04-02 12:23:34 +0700

    new tests

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,6 @@
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +13,17 @@ public class HelloWorldTest {
         Map<String, String> params = new HashMap<>();
         params.put("name", "Sergey");
 
-        Response response = RestAssured
+        JsonPath response = RestAssured
                 .given()
                 .queryParams(params)
                 .get("http://playground.learnqa.ru/api/hello")
-                .andReturn();
-        response.prettyPrint();
+                .jsonPath();
+
+        String name = response.get("answer");
+        if (name == null) {
+            System.out.println("The key 'answer2' is absent");
+        } else {
+            System.out.println(name);
+        }
     }
 }
