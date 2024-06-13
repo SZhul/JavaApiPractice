f7a9b797366646e644e64bcd27ee6cf4443e8aa9
Sergey Zhul <s.zhul@bftcom.com>
2024-06-13 07:09:49 +0700

    Token Test

---
+++ src/test/java/blockSecondExercises/LongResponsExercisesTest.java
@@ -0,0 +1,47 @@
+package blockSecondExercises;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import java.lang.Thread;
+
+public class LongResponsExercisesTest {
+
+    @Test
+    public void testLongResponse() throws InterruptedException {
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+
+        response.prettyPrint();
+        String getToken = response.path("token");
+        System.out.println(getToken);
+
+        Response responseWithToken = RestAssured
+                .given()
+                .queryParam("token", getToken)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .andReturn();
+
+        responseWithToken.prettyPrint();
+        String status = responseWithToken.path("status");
+
+        if (status.equals("Job is NOT ready")) {
+            Thread.sleep(16000);
+            Response nextResponseWithToken = RestAssured
+                    .given()
+                    .queryParam("token", getToken)
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .andReturn();
+            nextResponseWithToken.prettyPrint();
+            String endResult = nextResponseWithToken.path("result");
+            String endStatus = nextResponseWithToken.path("status");
+            if(endStatus.equals("Job is ready") && (endResult != null)) {
+                System.out.println("Задача выполнена");
+            } else {
+                System.out.println("Задача не выполнена");
+            }
+        }
+    }
+}
